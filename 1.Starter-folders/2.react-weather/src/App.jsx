92fd0bb9dc2d75a31bc8999dd8a6cc5f49858c5d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-16 15:45:38 +0100

    Ajout du CSS

--- 1.Starter-folders/2.react-weather/src/App.jsx
+++ 1.Starter-folders/2.react-weather/src/App.jsx
@@ -1,4 +1,5 @@
 import loader from "./assets/loader.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <p className="country-name">France</p>
       <p className="temperature">17°</p>
       <div className="info-icon-container">
-        <img src="/icons/01d.svg" alt="weather icon" />
+        <img src="/icons/01d.svg" alt="weather icon" className="info-icon" />
       </div>
     </main>
   );
