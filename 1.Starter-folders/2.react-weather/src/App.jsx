0a5eb6833066433cf7b07b99c6966f919098f5ab
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-16 15:36:22 +0100

    Meteo: Mise en place

--- 1.Starter-folders/2.react-weather/src/App.jsx
+++ 1.Starter-folders/2.react-weather/src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
- 
-  return (
+import loader from "./assets/loader.svg";
 
-      <main>
-      
-      </main>
-  
+function App() {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+
+      <p className="city-name">Paris</p>
+      <p className="country-name">France</p>
+      <p className="temperature">17°</p>
+      <div className="info-icon-container">
+        <img src="/icons/01d.svg" alt="weather icon" />
+      </div>
+    </main>
   );
 }
 
