3a0d6b166ace7970e9bf5cd9759e58bf629f0576
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-17 15:33:30 +0100

    Ajout de la fonctionnalite de changement de code

--- 1.Starter-folders/4.IDE/src/components/CodeTab.jsx
+++ 1.Starter-folders/4.IDE/src/components/CodeTab.jsx
@@ -1,6 +1,14 @@
+import { useDispatch } from "react-redux";
+import { updateCode } from "../features/tabs";
+
 export default function CodeTab({ code, id }) {
+  const dispatch = useDispatch();
+
+  console.log(updateCode);
+
   return (
     <textarea
+      onChange={(e) => dispatch(updateCode({ id, value: e.target.value }))}
       value={code}
       spellCheck="false"
       className="bg-zinc-900 text-slate-200 text-xl p-8
