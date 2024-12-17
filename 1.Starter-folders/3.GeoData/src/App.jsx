e2676a7b588c63fade0a67868af39543f10a7312
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-17 11:24:01 +0100

    Introduction et appel de l'API

--- 1.Starter-folders/3.GeoData/src/App.jsx
+++ 1.Starter-folders/3.GeoData/src/App.jsx
@@ -1,9 +1,42 @@
-function App() {
-  return (
-    <div>
+import { useState, useEffect } from "react";
 
+// https://restcountries.com/v3.1/region/europe
+
+function App() {
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/region/europe")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+
+        data.sort((a, b) => {
+          if (a.name.common < b.name.common) {
+            return -1;
+          } else if (a.name.common > b.name.common) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        setCountries(data);
+      })
+      .catch();
+
+    return () => {};
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-slate-800">
+      <div className="max-w-7xl mx-auto py-20 px-4">
+        <h1 className="text-gray-50 text-4xl">Europe Countries Data.</h1>
+        <p className="text-gray-100 text-xl mb-8">Click on a card to reveal a country's information.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
