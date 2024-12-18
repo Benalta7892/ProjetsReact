3296b5972bccfe4e795e06ec40606c993a1f6794
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-18 15:10:15 +0100

    Mise en place du store Redux

---
+++ 1.Starter-folders/5.chrono/src/features/chrono.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const chrono = createSlice({
+  name: "chrono",
+  initialState,
+  reducers: {},
+});
+
+// export const {  } = chrono.actions;
+export default chrono.reducer;
