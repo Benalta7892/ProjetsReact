ab6696e37a3a23cd20927290f6315a44db269ba9
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-19 16:02:19 +0100

    Gerer le tick et le reset complet

--- 1.Starter-folders/5.chrono/src/features/chrono.js
+++ 1.Starter-folders/5.chrono/src/features/chrono.js
@@ -7,7 +7,7 @@ const initialState = {
   },
   pause: {
     value: 300,
-    runningValue: 300,
+    runningValue: 2,
   },
   isPlaying: false,
   intervalID: undefined,
@@ -31,8 +31,8 @@ export const chrono = createSlice({
       if (action.payload.type === "session") {
         if (!state.isPlaying) {
           chosenState.value = chosenState.value + action.payload.value;
-          chosenState.runningValue = chosenState.value + action.payload.value;
-          state.displayedValue.value = chosenState.value;
+          chosenState.runningValue = chosenState.runningValue + action.payload.value;
+          state.displayedValue.value = chosenState.runningValue;
         } else {
           chosenState.value = chosenState.value + action.payload.value;
         }
@@ -40,7 +40,23 @@ export const chrono = createSlice({
         chosenState.value = chosenState.value + action.payload.value;
       }
     },
-    tick: (state, action) => {},
+    tick: (state, action) => {
+      if (state.session.runningValue > 0) {
+        state.session.runningValue--;
+        state.displayedValue.value = state.session.runningValue;
+        state.displayedValue.heading = "Work";
+      } else if (state.pause.runningValue > 0) {
+        state.pause.runningValue--;
+        state.displayedValue.value = state.pause.runningValue;
+        state.displayedValue.heading = "Pause";
+      } else {
+        state.cycles++;
+        state.session.runningValue = state.session.value - 1;
+        state.displayedValue.value = state.session.value - 1;
+        state.displayedValue.heading = "Work";
+        state.pause.runningValue = state.pause.value;
+      }
+    },
     setUpChrono: (state, action) => {
       state.isPlaying = true;
       state.intervalID = action.payload;
@@ -48,6 +64,10 @@ export const chrono = createSlice({
     resetChrono: (state, action) => {
       window.clearInterval(state.intervalID);
       state.isPlaying = false;
+      state.session.runningValue = state.session.value;
+      state.pause.runningValue = state.pause.value;
+      state.displayedValue.value = state.session.runningValue;
+      state.cycles = 0;
     },
   },
 });
@@ -55,7 +75,7 @@ export const chrono = createSlice({
 export function startChrono(action) {
   return function (dispatch, getState) {
     const intervalID = setInterval(() => {
-      dispatch(tick);
+      dispatch(tick());
     }, 1000);
 
     dispatch(setUpChrono(intervalID));
