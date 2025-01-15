e67fbbb9a99e49cf31b72b29ef59093d22833d2d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-15 17:53:43 +0100

    Mise en place et affichage des donnees

--- 1.Starter-folders/9.e-commerce/src/components/ProductsList.jsx
+++ 1.Starter-folders/9.e-commerce/src/components/ProductsList.jsx
@@ -11,5 +11,26 @@ export default function ProductsList() {
 
   console.log(products);
 
-  return <div>ProductsList</div>;
+  return (
+    <div className="x-6">
+      <h1 className="text-slate-100 text-2xl mb-6">Here are our products</h1>
+      <ul className="grid min-[500px]:grid-cols-2 md:grid-cols-3 gap-4">
+        {products.items &&
+          products.items.map((el) => (
+            <li key={el.id} className="p-4 bg-slate-200 rounded">
+              <img className="mb-4" src={`/images/${el.img}.png`} alt={el.title} />
+              <div className="flex justify-between items-center mb-6">
+                <p className="text-slate-700 text-lg">{el.title}</p>
+                <p className="text-slate-900 font-bold">{el.price}</p>
+              </div>
+              <button
+                className={`${el.picked ? "bg-green-700" : "bg-slate-600"} w-full
+                text-slate-100 px-2 inline-flex items-center justify-center rounded p-2 mr-2`}>
+                {el.picked ? "Item picked ✔️" : "Add to cart"}
+              </button>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
