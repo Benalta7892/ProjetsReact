258b20be71620869e0718f311554f4b1b5f52c7b
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-15 17:39:57 +0100

    Mise en place et affichage des donnees

--- 1.Starter-folders/9.e-commerce/src/components/ProductsList.jsx
+++ 1.Starter-folders/9.e-commerce/src/components/ProductsList.jsx
@@ -3,6 +3,12 @@ import { getProductsList } from "../features/products";
 
 export default function ProductsList() {
   const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  if (!products.items) {
+    dispatch(getProductsList());
+  }
+
   console.log(products);
 
   return <div>ProductsList</div>;
