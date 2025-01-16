e6e0f5989b1e5ba5d17f7a305ea98c57f7617eca
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-01-16 14:21:28 +0100

    Gerer le pannier

---
+++ 1.Starter-folders/9.e-commerce/src/features/cart.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { products } from "./products";
+
+const initialState = {
+  cartItems: [],
+};
+
+export const cart = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    createCartItem: (state, action) => {
+      state.cartItems.push(action.payload);
+    },
+  },
+});
+
+export function addOneToCart(action) {
+  return function (dispatch, getState) {
+    const storeState = getState();
+
+    const isAlreadyPresent = storeState.cart.cartItems.find((el) => el.id === action);
+
+    if (!isAlreadyPresent) {
+      const itemToAdd = storeState.products.items.find((el) => el.id === action.payload);
+
+      const newCartItem = {
+        ...itemToAdd,
+        quantity: 1,
+      };
+      dispatch(createCartItem(newCartItem));
+    }
+  };
+}
+
+export const { createCartItem } = cart.actions;
+export default cart.reducer;
